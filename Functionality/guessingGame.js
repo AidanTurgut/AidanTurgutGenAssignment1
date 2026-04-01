8a25b732dabed6ceb15966180b7a29c45f1790ce
Aidan Turgut <aidantur@gmail.com>
2026-04-01 18:14:42 -0500

    Story 4 Task 1: Modified how guessing game works

--- Functionality/guessingGame.js
+++ Functionality/guessingGame.js
@@ -1,12 +1,12 @@
 function guessFunc(choice){
     let guessSection=document.getElementById("guessResult");
-    if(choice==1){
+    if(choice=='Dog'){
         guessSection.innerHTML="You guessed correctly!";
-        guessSection.innerHTML.style.color="green";
-    }else if(choice==2){
+        guessSection.style.color="green";
+    }else if(choice=='Cat'){
         guessSection.innerHTML="You guessed incorrectly!";
-        guessSection.innerHTML.style.color="red";
-    }else if(choice==3){
+        guessSection.style.color="red";
+    }else if(choice=='Bird'){
         guessSection.innerHTML="You guessed incorrectly!";
         guessSection.innerHTML.style.color="red";
     }
