00092df31b7a4de87cb4e02db5887247756b153c
Aidan Turgut <aidantur@gmail.com>
2026-04-01 15:39:08 -0500

    Story 2 Task 3: added more operations

--- Functionality/calculations.js
+++ Functionality/calculations.js
@@ -8,3 +8,17 @@ function subtract(){
     let num2 = parseFloat(document.getElementById("subtractionInput2").value);
     document.getElementById("subtractionResult").textContent = num1 - num2;
 }
+function multiply(){
+    let num1 = parseFloat(document.getElementById("multiplicationInput1").value);
+    let num2 = parseFloat(document.getElementById("multiplicationInput2").value);
+    document.getElementById("multiplicationResult").textContent = num1 * num2;
+}
+function divide(){ 
+    let num1 = parseFloat(document.getElementById("divisionInput1").value);
+    let num2 = parseFloat(document.getElementById("divisionInput2").value);
+    if(num2 === 0){
+        document.getElementById("divisionResult").textContent = "Cannot divide by zero";
+    } else {
+        document.getElementById("divisionResult").textContent = num1 / num2;
+    }
+}
